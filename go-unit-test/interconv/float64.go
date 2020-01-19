61e5f7300341eb93052e8d06b91761bbb0b772c7
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2020-01-20 04:15:32 +0700

    added golang unit testing latihan

---
+++ go-unit-test/interconv/float64.go
@@ -0,0 +1,6 @@
+package interconv
+
+// ParseFloat64 function
+func ParseFloat64(val interface{}) (float64, error) {
+	return 1, nil
+}
---
+++ go-unit-test/interconv_test/float64_test.go
@@ -0,0 +1,12 @@
+package interconv_test
+
+import (
+	"testing"
+)
+
+func TestParseFloat64(t *testing.T) {
+	t.Run("Nil Input", func (t *testing.T) {
+		var i interface{}
+		convertedNum, _ := interconv.
+	})
+}
