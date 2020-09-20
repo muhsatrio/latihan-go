35f88cd39590ec505f8242bbb9dac4ca54c00cc5
muhsatrio <muhsatrio@gmail.com>
2020-09-20 16:36:12 +0700

    feat: function closure

--- belajar-golang-novalagung/main.go
+++ belajar-golang-novalagung/main.go
@@ -54,6 +54,18 @@ func myHobbies(name string, hobbies ...string) {
 	fmt.Printf("My hobbies are: %s\n", convertHobbiesToString)
 }
 
+func findMax(numbers []int, max int) (int, func() []int) {
+	var res []int
+	for _, e := range numbers {
+		if e <= max {
+			res = append(res, e)
+		}
+	}
+	return len(res), func() []int {
+		return res
+	}
+}
+
 func main() {
 	fmt.Println("hello world")
 
@@ -332,7 +344,7 @@ func main() {
 	fmt.Printf("luas lingkaran: %.2f\n", area)
 	fmt.Printf("keliling lingkaran: %.2f\n", circumference)
 
-	// Penggunaan fungsi variadic
+	// Penggunaan fungsi variadic: fungsi dengan parameter sejenis yang tak terbatas
 
 	var avg = calculateAverage(1, 2, 3, 4, 5)
 	var numbers = []int{
@@ -346,4 +358,50 @@ func main() {
 		"tidur",
 	}
 	myHobbies("satrio", listHobbies...)
+
+	// Penggunaan fungsi closure: fungsi yang disimpan di dalam variabel
+
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+		return min, max
+	}
+
+	numbers = []int{1, 2, 3, 4, 5, 6, 7, 8}
+	var min, max = getMinMax(numbers)
+	fmt.Printf("data: %v\nmin: %d, max: %d\n", numbers, min, max)
+
+	// Penggunaan fungsi closure yang bisa dieksekusi langsung saat deklarasinya
+
+	numbers = []int{1, 2, 3, 4, 5}
+
+	var newNumbers = func(min int) []int {
+		var r []int
+		for _, e := range numbers {
+			if e < min {
+				r = append(r, e)
+			}
+		}
+		return r
+	}(3)
+
+	fmt.Printf("Original numbers: %v\n", numbers)
+	fmt.Printf("New numbers: %v\n", newNumbers)
+
+	// Penggunaam fungsi closure sebagai return
+
+	var lengthNum, getNumbers = findMax(numbers, 3)
+
+	fmt.Println(lengthNum)
+	fmt.Println(getNumbers())
+
 }
