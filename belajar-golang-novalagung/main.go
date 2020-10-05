be4797d5aa925b52fe56cf0f3fc79d3f80511a0b
muhsatrio <muhsatrio@gmail.com>
2020-10-06 00:33:57 +0700

    feat: interface

--- belajar-golang-novalagung/main.go
+++ belajar-golang-novalagung/main.go
@@ -51,6 +51,41 @@ func (e Employee) getNameAt(i int) string {
 	return strings.Split(e.name, " ")[i-1]
 }
 
+// lesson interface
+
+type hitung interface {
+	luas() float64
+	keliling() float64
+}
+
+type lingkaran struct {
+	diameter float64
+}
+
+type persegi struct {
+	sisi float64
+}
+
+func (l lingkaran) jariJari() float64 {
+	return l.diameter / 2
+}
+
+func (l lingkaran) luas() float64 {
+	return math.Pi * math.Pow(l.jariJari(), 2)
+}
+
+func (l lingkaran) keliling() float64 {
+	return math.Pi * l.diameter
+}
+
+func (p persegi) keliling() float64 {
+	return p.sisi * 4
+}
+
+func (p persegi) luas() float64 {
+	return math.Pow(p.sisi, 2)
+}
+
 // Method Pointer
 
 func (e Employee) changeName1(name string) {
@@ -594,4 +629,25 @@ func main() {
 	emply.sayHello()
 	emply.changeName2("satrio wicaksono")
 	emply.sayHello()
+
+	// lesson interface
+
+	var bangunDatar hitung
+
+	bangunDatar = persegi{12}
+
+	fmt.Println("persegi")
+	fmt.Println("luas: ", bangunDatar.luas())
+	fmt.Println("keliling: ", bangunDatar.keliling())
+
+	bangunDatar = lingkaran{12}
+
+	fmt.Println("lingkaran")
+	fmt.Println("luas: ", bangunDatar.luas())
+	fmt.Println("keliling: ", bangunDatar.keliling())
+
+	// variabel dibawah dicasting terlebih dahulu ke tipe lingkaran
+
+	fmt.Println("jari-jari: ", bangunDatar.(lingkaran).jariJari())
+
 }
